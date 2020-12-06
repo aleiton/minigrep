0e6ba4bbd378d224080fd7ed9150515ec0ea9c1d
aleiton <aleiton.design@gmail.com>
2020-12-06 16:19:05 -0300

    Modularize configuration

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -14,4 +13,18 @@ fn main() {
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
